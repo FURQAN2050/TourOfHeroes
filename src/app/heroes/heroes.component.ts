ff058d39079cb13aaba0e6f8a8021cae5bc63ac6
MUHAMMAD <FURQAN>
2019-03-09 15:52:29 +0500

    learn how to use *ngFor in angular 2+. use *ngIf. use dynamic css class,

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,18 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import {Hero}  from '../hero';
+import {HEROES} from '../mock-heroes'
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.scss']
 })
 export class HeroesComponent implements OnInit {
-  hero :Hero={
-    id:1,
-    name:"furqan",
-  }
+  // hero :Hero={
+  //   id:1,
+  //   name:"furqan",
+  // }
+  heroes=HEROES;
+  selectedHero:Hero;
   constructor() { }
 
   ngOnInit() {
   }
-
+  selectHero(hero){
+    this.selectedHero=hero;
+    console.log('selected hero',hero)
+  }
 }
